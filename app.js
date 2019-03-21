197c8d54cb7175a735a36437b3a667bd77f358e6
joshuaking06 <joshua.king0692@gmail.com>
2019-03-21 16:31:16 +0000

    changed shade of green

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const arrows = document.querySelectorAll('.arrow')
 
 arrows.forEach((arrow) => {
 	arrow.addEventListener('click', (e) => {
-		slideOut(current, e.target.id)
+		projects[current].classList.toggle('hidden')
 		changeCurrent(e.target.id)
 		changeProject(current)
 	})
@@ -14,28 +14,8 @@ const changeCurrent = (direction) => {
 	direction === 'left' ? (current -= 1) : current++
 	if (current < 0) current = projects.length - 1
 	if (current > projects.length - 1) current = 0
-	console.log(current)
-	slideIn(current, direction)
-}
-
-const slideOut = (current, direction) => {
-	projects[current].classList.remove(`slide-out-${direction}`)
-	projects[current].classList.add(`slide-out-${direction}`)
-}
-
-const slideIn = (current, direction) => {
-	projects[current].classList.remove(`slide-in-${direction}`)
-	projects[current].classList.add(`slide-in-${direction}`)
 }
 
 const changeProject = (current) => {
-	projects.forEach((project, index) => {
-		if (index === current) {
-			project.classList.add('visible')
-			project.classList.remove('hidden')
-		} else {
-			project.classList.add('hidden')
-			project.classList.remove('visible')
-		}
-	})
+	projects[current].classList.toggle('hidden')
 }
