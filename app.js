43df92252ef9c7f65b1f5c8259d7ffd12bd50502
joshuaking06 <joshua.king0692@gmail.com>
2019-03-21 18:21:03 +0000

    added hr styles

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ let current = 0
 const projects = document.querySelectorAll('.projects')
 const arrows = document.querySelectorAll('.arrow')
 
+// smooth scroll effect
 document.querySelectorAll('a').forEach((anchor) => {
 	anchor.addEventListener('click', function(e) {
 		e.preventDefault()
@@ -12,6 +13,7 @@ document.querySelectorAll('a').forEach((anchor) => {
 	})
 })
 
+// when you click the arrows
 arrows.forEach((arrow) => {
 	arrow.addEventListener('click', (e) => {
 		projects[current].classList.toggle('hidden')
@@ -21,12 +23,14 @@ arrows.forEach((arrow) => {
 	})
 })
 
+// changing the current project to show
 const changeCurrent = (direction) => {
 	direction === 'left' ? (current -= 1) : current++
 	if (current < 0) current = projects.length - 1
 	if (current > projects.length - 1) current = 0
 }
 
+// showing the newly selected project
 const changeProject = (current) => {
 	projects[current].classList.toggle('hidden')
 	projects[current].style.transition = 'opacity .5s linear'
